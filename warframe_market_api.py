ee9d23a2172cf3658c1e5da239dd426c03a3c363
briantran818 <briatran@pdx.edu>
2026-02-24 17:37:04 -0800

    Aggregated Data according to item list
    Secondary call to API to obtain item list, mapped item list IDs to Dataframe and replaced with names of items.

--- warframe_market_api.py
+++ warframe_market_api.py
@@ -2,26 +2,46 @@ import requests
 import json
 import pandas as pd
 
-wfm_url = "https://api.warframe.market/v2/orders/recent"
+order_url = "https://api.warframe.market/v2/orders/recent"
+item_url = "https://api.warframe.market/v2/items"
 headers = {
     "Platform" : "pc",
     "Language" : "en",
     'content-type' : 'application/json'
 }
 
-response = requests.get(wfm_url, headers=headers)
+order_response = requests.get(order_url, headers=headers)
 
 try:
-    response.raise_for_status()  # Check if the request was successful
+    order_response.raise_for_status()  # Check if the request was successful
 except requests.exceptions.HTTPError as e:
     print("HTTP error occurred: ", e)
 
-response_dict = json.loads(response.text)
-orders = response_dict["data"]
+
+order_response_dict = json.loads(order_response.text)
+orders = order_response_dict["data"]
 df = pd.json_normalize(orders)
 
 df = df.drop(columns=[col for col in df.columns if col.startswith("user.")])
 df = df.drop(columns=["id", "perTrade", "visible", "createdAt", "updatedAt", "subtype"])
 
-buy_df = df[df["type"] == "buy"]
-sell_df = df[df["type"] == "sell"]
+item_response = requests.get(item_url, headers=headers)
+try:
+    item_response.raise_for_status()  # Check if the request was successful
+except requests.exceptions.HTTPError as e:
+    print("HTTP error occurred: ", e)
+
+item_response_dict = json.loads(item_response.text)
+item_list = item_response_dict["data"]
+item_df = pd.json_normalize(item_list)
+
+df["item_name"] = df["itemId"].map(item_df.set_index("id")["i18n.en.name"])
+
+aggregated_df = df.groupby(["type", "item_name"]).agg(
+        quantity=("quantity", "sum"),
+        average_price=("platinum", "mean"),
+        lowest_price=("platinum", "min"),
+        highest_price=("platinum", "max")   
+).reset_index()
+
+print(aggregated_df.head(n=30))
