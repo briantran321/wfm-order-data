12b5e1d63819965087a4238c8e2271bce67f9069
briantran818 <briatran@pdx.edu>
2026-02-24 19:14:30 -0800

    GUI Added
    Added GUI setup to read dataframes and display within an application.

--- warframe_market_api.py
+++ warframe_market_api.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import pandas as pd
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QWidget, QVBoxLayout
+from PyQt6.QtCore import QAbstractTableModel, Qt
 
 # Calls upon warframe market api and returns a json object.
 def api_call(url, custom_header):
@@ -17,6 +19,47 @@ def normalize_df(response_dict):
     orders = response_dict["data"]
     df = pd.json_normalize(orders)
     return df
+    
+# PyQt6 DataFrame GUI reads in dataframe
+class PandasModel(QAbstractTableModel):
+    def __init__(self, df):
+        super().__init__()
+        self._df = df
+
+    def rowCount(self, parent=None):
+        return self._df.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._df.shape[1]
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if role == Qt.ItemDataRole.DisplayRole:
+            value = self._df.iloc[index.row(), index.column()]
+            return str(value)
+        return None
+
+    def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return str(self._df.columns[section])
+            else:
+                return str(self._df.index[section])
+        return None
+
+# GUI layout for the application
+class DataFrameWindow(QMainWindow):
+    def __init__(self, df):
+        super().__init__()
+        self.setWindowTitle("WFM Orders")
+        self.resize(900, 600)
+        widget = QWidget()
+        layout = QVBoxLayout()
+        self.table = QTableView()
+        self.model = PandasModel(df)
+        self.table.setModel(self.model)
+        layout.addWidget(self.table)
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
 def main():
     order_url = "https://api.warframe.market/v2/orders/recent" # API endpoint for 500 recent orders
@@ -43,9 +86,12 @@ def main():
             highest_price=("platinum", "max")   
     ).reset_index()
 
-    print(aggregated_df.head(n=30))
-
-    return 0
+    # Show GUI
+    app = QApplication([])
+    window = DataFrameWindow(aggregated_df)
+    window.show()
+    app.exec()
 
 if __name__ == "__main__":
     main()
+
